ed4574555b245f36f6decc069cda4977456d524f
Gestan <104741659+Gestan24@users.noreply.github.com>
2022-10-08 16:53:17 -0500

    added tests for contact component

--- src/components/Gallery/__tests__/index.test.js
+++ src/components/Gallery/__tests__/index.test.js
@@ -11,15 +11,15 @@ describe('Gallery is rendering', () => {
 
     it('renders', () => {
         render(<Gallery currentCategory={portrait} />);
+    })
+
+    it('matches snapshot', () => {
+        const { asFragment } = render(<Gallery currentCategory={portrait} />)
+        expect(asFragment()).toMatchSnapshot()
+    })
+    
+    it('renders', () => {
+        const { getByTestId } = render(<Gallery currentCategory={portrait} />)
+        expect(getByTestId('h1tag')).toHaveTextContent('Portraits')
     });
-})
-
-it('matches snapshot', () => {
-    const { asFragment } = render(<Gallery currentCategory={portrait} />)
-    expect(asFragment()).toMatchSnapshot()
-})
-
-it('renders', () => {
-    const { getByTestId } = render(<Gallery currentCategory={portrait} />)
-    expect(getByTestId('h1tag')).toHaveTextContent('Portraits')
 })
