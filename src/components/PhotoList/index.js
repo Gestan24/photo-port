3656e073434e4aeaea4bce6da319133bc78bbde3
Gestan <104741659+Gestan24@users.noreply.github.com>
2022-10-08 18:25:06 -0500

    added onClose to be able to toggle the modal whenever the close button is clicked

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -109,14 +109,15 @@ const PhotoList = ({ category }) => {
   const currentPhotos = photos.filter((photo) => photo.category === category);
 
   const toggleModal = (image, i) => {
-    // current photo
-    setCurrentPhoto({...image, index: i})
-    setIsModalOpen(true);
+    setCurrentPhoto({ ...image, index: i });
+    setIsModalOpen(!isModalOpen);
   }
 
   return (
     <div>
-      {isModalOpen && <Modal currentPhoto={currentPhoto} />}
+      {isModalOpen && (
+        <Modal currentPhoto={currentPhoto} onClose={toggleModal} />
+      )}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
